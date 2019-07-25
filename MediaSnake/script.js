058e487b5b92e4d7f6e581b0e1da1359f79239e2
Egor <likeroge@gmail.com>
2019-07-25 14:00:03 +0400

    MediaSnake game field added

---
+++ MediaSnake/script.js
@@ -0,0 +1,20 @@
+//canvas
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
+
+//vars
+let score = document.querySelector(".score");
+
+
+//test
+context.fillStyle = "green";
+context.fillRect(10,10,20,20);
+score.innerHTML = 777;
+
+
+//Func for start btn
+//....
+
+
+//Game Process
+//.....
